f45de872e232bce17c9c170e36fe610115ab5bd3
Cesar Rodriguez <59242247+Cesar001-co@users.noreply.github.com>
2025-01-02 11:02:23 -0500

    Enititys franquicia, sucursal getters and setters

--- franquicias/src/main/java/com/franquiciasBackend/franquicias/entitys/Franquicia.java
+++ franquicias/src/main/java/com/franquiciasBackend/franquicias/entitys/Franquicia.java
@@ -17,4 +17,34 @@ public class Franquicia {
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "franquicia")
     private List<Sucursal> sucursales;
+
+    public Franquicia(long idFranquicia, String nombre, List<Sucursal> sucursales) {
+        this.idFranquicia = idFranquicia;
+        this.nombre = nombre;
+        this.sucursales = sucursales;
+    }
+
+    public long getIdFranquicia() {
+        return idFranquicia;
+    }
+
+    public void setIdFranquicia(long idFranquicia) {
+        this.idFranquicia = idFranquicia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Sucursal> getSucursales() {
+        return sucursales;
+    }
+
+    public void setSucursales(List<Sucursal> sucursales) {
+        this.sucursales = sucursales;
+    }
 }
--- franquicias/src/main/java/com/franquiciasBackend/franquicias/entitys/Sucursal.java
+++ franquicias/src/main/java/com/franquiciasBackend/franquicias/entitys/Sucursal.java
@@ -21,4 +21,43 @@ public class Sucursal {
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "sucursal")
     private List<Producto> productos;
+
+    public Sucursal(Long idSucursal, String nombre, Franquicia franquicia, List<Producto> productos) {
+        this.idSucursal = idSucursal;
+        this.nombre = nombre;
+        this.franquicia = franquicia;
+        this.productos = productos;
+    }
+
+    public Long getIdSucursal() {
+        return idSucursal;
+    }
+
+    public void setIdSucursal(Long idSucursal) {
+        this.idSucursal = idSucursal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Franquicia getFranquicia() {
+        return franquicia;
+    }
+
+    public void setFranquicia(Franquicia franquicia) {
+        this.franquicia = franquicia;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
 }
